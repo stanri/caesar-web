04dab6351dbd25b3163929e8712a19c378813bb4
Pei Tao <peitao@mit.edu>
2014-07-03 14:58:35 -0400

    CAESARRRRRRRR

--- dashboard/views.py
+++ dashboard/views.py
@@ -26,11 +26,10 @@ import logging
 def notificationSeen(request):
     if request.method == "POST":
         noteID = request.POST["notification_id"]
-        note = Notification.objects.filter(id=noteID)
-        note.seen = True
-        note.save()
-    
-    return HttpResponse("ASDFGHJKL")
+        note = Notification.objects.get(id=noteID)
+        if note.recipient.user == request.user:
+            note.seen = True
+            note.save()
 
 
 #METHODS FOR URLS
@@ -66,7 +65,8 @@ def dashboard(request):
 def all_activity(request):
     user = request.user
     maxNotifications = 1000
-    my_code_notifications_all, other_code_notifications_all = get_recent_notifications(user, maxNotifications)
+    my_code_notifications_all = get_recent_notifications(user, maxNotifications)
+    other_code_notifications_all = get_recent_notifications(user, maxNotifications)
     return render(request, 'dashboard/activity.html', {
         'my_code_notifications_all': my_code_notifications_all,
         'other_code_notifications_all': other_code_notifications_all
@@ -165,12 +165,11 @@ def create_recent_activity_list(*args):
     return recent_activity[::-1] #we reverse the list, because the list has 0 index as earliest action.
 
 ## End of unused methods
-##
+## 
 
 '''
-Returns most recent unseen notifications on the users' code and others' code related to the user.
-Returns TWO lists of tuples (notification, code snippet)
-Can change maxNotifications to get all notifications as well.
+Returns a list of the five most recent and unseen notifications on the users' code and a list of the five most recent
+and unseen notifications on others' code that the user is related to.
 '''
 def get_recent_notifications(dashboard_user, maxNotifications = 5):
     my_notifications = []
@@ -188,7 +187,7 @@ def get_recent_notifications(dashboard_user, maxNotifications = 5):
     return add_snippets(my_notifications), add_snippets(other_notifications)
 
 '''
-Turns a list of notifications into list of tuples in the form (notification, snippet).
+Turns a list of notifications into list of tuples (notification, snippet)
 '''
 def add_snippets(notifications):
     notifications_with_snippets = []
@@ -201,6 +200,8 @@ def add_snippets(notifications):
                 snippet = snippet[:snippet_max_len]
         notifications_with_snippets.append( (notif,snippet) )
     return notifications_with_snippets
+
+
 
 def dashboard_for(request, dashboard_user, new_task_count = 0, allow_requesting_more_tasks = False):
     def annotate_tasks_with_counts(tasks):
